569c62294187bf932d8b75d6a612b7cd3b4b26c4
vishal-mourya <vishal.maurya2050@gmail.com>
2021-04-23 15:33:29 +0530

    Task From Vishal Mourya, Frontend-Flutter

---
+++ vishal-mourya-frontend-task/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kAppBarTextStyleTitle = TextStyle(color: Colors.white, fontSize: 15.0);
+const kAppBarTextStyleBody =
+    TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold);
+
+const kAppPrimaryColor = Color(0xFF7CDC41);
