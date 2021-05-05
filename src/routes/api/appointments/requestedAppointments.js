57f420086017a582d6ba167f5d8ca528b05f3b25
ayushag99 <ayush.ashuvaish@gmail.com>
2021-05-05 12:41:15 +0530

    dev: add feature to view requested appointment both patient and doctor

--- src/routes/api/appointments/requestedAppointments.js
+++ src/routes/api/appointments/requestedAppointments.js
@@ -7,16 +7,20 @@ router.get(
 	"/",
 	passport.authenticate("jwt", { session: false,  failureRedirect:"./login" }),
 	(req, res) => {
-        const search={}
+        const query={}
         if(req.user.role === "DOCTOR"){
-            return
+            query.search={doctor: req.user.id}
+            query.populate='patient'
+            query.schema='doctor'
         }else{
-            search.patient= req.user.id
+            query.search={patient: req.user.id}
+            query.populate='doctor'
+            query.schema='patient'
         }
-        Appointment.find(search).populate('doctor').then(
+        Appointment.find(query.search).populate(query.populate).then(
             appointments=>{
                     console.log(appointments)
-                res.render("requestedAppointments",{appointments});
+                res.render("requestedAppointments",{appointments,schema:query.schema});
             }
         ).catch()
 		
