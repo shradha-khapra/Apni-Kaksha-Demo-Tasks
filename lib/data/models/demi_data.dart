b8005d11b7db22c986b1207d559ecc661babcfaa
Jivansh Sharma <jivansh17@gmail.com>
2021-06-24 21:05:14 +0530

    feat: add models for prodcuts and demi_data

---
+++ lib/data/models/demi_data.dart
@@ -0,0 +1,39 @@
+import 'package:shopit/data/models/product.dart';
+
+class DummyDataService {
+  List<Product> _riceList = [
+    Product(
+      productId: 1,
+      productName: "Basmati Rice",
+      description: "Gold Standard Rice Fit For All",
+      productPrice: 299,
+      imagePath: "assets/images/basmati.jpg",
+      quantityInCart: 0,
+    ),
+    Product(
+      productId: 2,
+      productName: "Daawat Rice",
+      description: "Gold Standard Rice Fit For All",
+      productPrice: 249,
+      imagePath: "assets/images/daawat.jpg",
+      quantityInCart: 0,
+    ),
+    Product(
+      productId: 3,
+      productName: "Hemani Rice",
+      description: "Gold Standard Rice Fit For All",
+      productPrice: 199,
+      imagePath: "assets/images/hemani.jpeg",
+      quantityInCart: 0,
+    ),
+    Product(
+      productId: 4,
+      productName: "Khusbhoo Rice",
+      description: "Gold Standard Rice Fit For All",
+      productPrice: 379,
+      imagePath: "assets/images/khushbhoo.jpg",
+      quantityInCart: 0,
+    ),
+  ];
+  List<Product> getRiceProducts() => _riceList;
+}
---
+++ lib/data/models/product.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class Product {
+  final String? productName, description, imagePath;
+  final int? productId, productPrice, quantityInCart;
+
+  Product(
+      {this.productName,
+      this.description,
+      this.imagePath,
+      this.productId,
+      this.productPrice,
+      this.quantityInCart});
+}
